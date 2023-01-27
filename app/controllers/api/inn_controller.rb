511fe96fe470f5330655e0d13207d0bda9a041f5
dvitvitskiy <dmitriy.vitvitskiy@sbermarket.ru>
2023-01-27 03:19:14 +0300

    fix

--- app/controllers/api/inn_controller.rb
+++ app/controllers/api/inn_controller.rb
@@ -6,6 +6,7 @@ class Api::InnController < ApplicationController
   api :GET, '/inn/:inn', 'Проверка ФЛ на долги - json: {"error": 0, "finesList": [], "count": null, "inGarage": 0} (https://gosnalogi.ru/ajax/taxes_inn)'
   def show
     json = JSON.parse RestClient.get("https://gosnalogi.ru/ajax/taxes_inn?inn=#{params[:id]}")
+    json['error'] = json['error'].to_i
     render status: :ok, json: json
   end
 end
